1a2ebc2c150891e297d9c94bbf2094c5fedcde19
andrejs-poikans <andrejs.poikans@gmail.com>
2026-04-01 21:25:32 +0200

    decap cms added

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [preact()]
+  prefetch: {
+    prefetchAll: true,
+  },
+  integrations: [preact()],
 });
