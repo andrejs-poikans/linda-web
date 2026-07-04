1fc060918f23d6949d9b8a6248b218247312c868
andrejs-poikans <andrejs.poikans@gmail.com>
2026-07-04 13:43:21 +0300

    prefetch removed

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -8,8 +8,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  prefetch: {
-    prefetchAll: true,
-  },
   integrations: [preact()],
 });
