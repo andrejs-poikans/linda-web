d0655bfe32bf1501260e219d0533cdef76f5ba94
andrejs-poikans <andrejs.poikans@gmail.com>
2026-07-09 15:00:07 +0300

    sitemap added

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,11 +2,17 @@
 import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
-
+import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lindamence.com",
   adapter: netlify(),
-  integrations: [preact()],
+  integrations: [
+    preact(),
+    sitemap({
+      filter: (page) => !page.includes("/admin"),
+    }),
+  ],
 });
